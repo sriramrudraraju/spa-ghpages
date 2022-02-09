a159314950753853c71d51f91938339e706ed641
sriramrudraraju <sriram.rudraraju6@gmail.com>
2022-02-08 23:13:37 -0600

    feat: react router support

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SPA on github pages
 
 [Demo](https://sriramrudraraju.github.io/spa-ghpages/)
+
+### Deploy to gh-pages
+* When code pushed to main, git actions will build and deploy the code to gh-pages.
+* Or manually run `npm run deploy` to deploy the code to gh-pages.
+
+### Routing for react app
+* React router [setup](https://v5.reactrouter.com/web/guides/quick-start/installation)
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
@@ -19,8 +25,45 @@ function App() {
           Learn React 1
         </a>
       </header>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
